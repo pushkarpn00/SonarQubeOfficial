fc27cb5176b60934496a3b7d380593528fa881ba
pushkarpn00 <pushkarpn00@gmail.com>
2021-09-18 20:17:31 +0530

    First Commit

---
+++ src/main/java/com/acme/basic/SonarQubeDemo.java
@@ -0,0 +1,13 @@
+package com.acme.basic;
+
+public class SonarQubeDemo {
+
+  void sayHello() {
+    System.out.println("Hello World!");
+  }
+
+  void notCovered() {
+    System.out.println("This method is not covered by unit tests");
+  }
+
+}
---
+++ src/test/java/com/acme/basic/SonarQubeDemoTest.java
@@ -0,0 +1,11 @@
+package com.acme.basic;
+
+import org.junit.Test;
+
+public class SonarQubeDemoTest {
+
+  @Test
+  public void sayHello() {
+    new SonarQubeDemo().sayHello();
+  }
+}
